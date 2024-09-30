dc59b0a26447f27511351c77f535c745cb6e6c39
Alpha <104311979+Alfa-Akbarali@users.noreply.github.com>
2024-09-30 18:15:50 +0500

    changed rezume.jsx

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Style_rezume.scss';
 import ResumeImg from '../../assets/ResumeImg.jpg';
-import RezumeFile from '../../assets/Resumefile.pdf'
 const Resume = () => {
   document.addEventListener('contextmenu', event => {
     if (event.target.nodeName === 'IMG') {
@@ -12,12 +11,14 @@ const Resume = () => {
   return (
     <div className='resume_container'>
       <div className='image_box'>
-        <img src={ResumeImg} alt="img" />
+        <Link to="http://localhost:3000/static/media/Resumefile.c259ce161cb80ddfcd60.pdf" target="_blank" rel="noopener noreferrer">
+          <img src={ResumeImg} alt="img" />
+        </Link>
       </div>
       <div className='link_box'>
         <div>
-          <p>To download my Resume:</p>
-          <a className='resume_downloader' href={RezumeFile}>Download</a>
+          {/* <p>To download my Resume:</p> */}
+          {/* <a className='resume_downloader' href={RezumeFile}>Download</a> */}
         </div>
         <Link className='back_btn' to="/">Home</Link>
       </div>
